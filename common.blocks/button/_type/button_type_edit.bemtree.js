a102dcbb914a3d439f6c6c3c75d10d80bc90263b
Constantine Lopatin <k.n.lopatin@gmail.com>
2016-09-22 13:40:59 +0300

    Refactor button type edit

--- common.blocks/button/_type/button_type_edit.bemtree.js
+++ common.blocks/button/_type/button_type_edit.bemtree.js
@@ -1,21 +1,20 @@
 block('button').mod('type', 'edit')(
     def()(function () {
-        var ctx = this.ctx.id && this.data.user && this.data.user.IsAdmin?
-            {
-                block : 'control-group',
-                mods : { type : 'edit' },
-                content: [
-                    {
-                        block : 'button',
-                        mods : { type : 'link', view : 'plain' },
-                        url : '?edit_mode=true&id=' + this.ctx.id,
-                        icon : {
-                            block : 'icon',
-                            url : '/img/edit-icon.png'
+        if (this.ctx.id && this.data.user && this.data.user.IsAdmin)
+            return applyCtx({
+                    block : 'control-group',
+                    mods : { type : 'edit' },
+                    content: [
+                        {
+                            block : 'button',
+                            mods : { type : 'link', view : 'plain' },
+                            url : '?edit_mode=1&id=' + this.ctx.id,
+                            icon : {
+                                block : 'icon',
+                                url : '/img/edit-icon.png'
+                            }
                         }
-                    }
-                ]
-            } : {};
-        return applyCtx(ctx);
+                    ]
+                });
     })
 );
