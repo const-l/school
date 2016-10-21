ce99544bc56760335b4f8c361dcc3664b5f7b660
Constantine Lopatin <k.n.lopatin@gmail.com>
2016-10-21 15:13:34 +0300

    Control commands
    - build
    - start
    - clean

--- production/production.js
+++ production/production.js
@@ -1,32 +1,20 @@
-var fs = require('fs');
 require('../server/utils')()
-    .chain([
-        function (defer) {
-            fs.readFile('./production/production.json', function (err, data) {
-                if (err) return defer.reject(err);
-                try {
-                    data = JSON.parse(data);
-                    Object.assign(process.env, data);
-                }
-                catch (e) { return defer.reject(e); }
-                defer.resolve();
-            });
-        },
-        function (defer) {
-            require('enb')
-                .make()
-                .then(
-                    function() { defer.resolve(); },
-                    function() { defer.reject(arguments); }
-                );
-        }
-    ], this)
-    .then(
+    .async(function (defer) {
+        require('fs').readFile('./production/production.json', function (err, data) {
+            if (err) return defer.reject(err);
+            try {
+                data = JSON.parse(data);
+                Object.assign(process.env, data);
+            }
+            catch (e) { return defer.reject(e); }
+            defer.resolve();
+        });
+    }, this).then(
         function () {
             require('../server/app');
         },
-        function () {
-            /// TODO. Need logger
-            console.error(arguments);
+        function (err) {
+            console.error('Start error', err.stack);
+            console.trace('server stack');
         }
     );
